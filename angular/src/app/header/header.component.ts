c7974d41b572734e3afb9e8672afc5c2780022c5
NikoletaDalaktchieva <nikoletadal@gmail.com>
2018-04-17 16:31:27 +0300

    Header

---
+++ angular/src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  logo = require('./img/logo1.png');
+  search = require('./img/search.png');
+  logout = require('./img/logout.png')
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
